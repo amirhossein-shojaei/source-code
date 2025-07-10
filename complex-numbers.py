2fb6c434417eccaf5a6ec21d66c9fe02a7020860
amirhossein-shojaei <a.sh.busines@gmail.com>
2025-07-10 18:59:40 +0330

    Deleting the complex-numbers.py file from the source code

--- complex-numbers.py
+++
@@ -1,6 +0,0 @@
-#Creating complex numbers
-
-real = int(input('input the real part'))
-imag = int(input('input the imag part'))
-complex_text = complex(real, imag)
-print(complex_text) #(complex)ایجاد اعداد مختلط
