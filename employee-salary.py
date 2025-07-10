916224d8a6dfcf0dead035d82bdee83999020b99
amirhossein-shojaei <a.sh.busines@gmail.com>
2025-07-10 19:03:45 +0330

    Deleting the employee-salary.py file from the source code

--- employee-salary.py
+++
@@ -1,7 +0,0 @@
-#Employee salary after deduction of taxes and insurance costs
-
-salary = int(input('input your salary: '))
-bime = (salary * 7) / 100
-maliat = (salary * 10) / 100
-daryafti = salary - (bime + maliat)
-print('Bime is: ', bime, '\nmaliat is: ', maliat, '\ndaryafti is: ', daryafti)
