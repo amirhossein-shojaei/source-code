117ece99388d5bd3343d2d978c720aedf379e0b6
amirhossein-shojaei <a.sh.busines@gmail.com>
2025-07-10 19:07:43 +0330

    Deleting the string-repetitions.py file from the source code

--- string-repetitions.py
+++
@@ -1,7 +0,0 @@
-#The number of string repetitions
-
-string = input('input the string: ')
-string += ' '
-tedad_tekrar = int(input('input the tedad tekrar: '))
-new_string = string * tedad_tekrar
-print(new_string) #تعداد تکرار رشته
