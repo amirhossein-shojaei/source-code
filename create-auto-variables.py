fb7a7fc029274b9ac294038483e0c67167fc3943
amirhossein-shojaei <a.sh.busines@gmail.com>
2025-07-10 19:01:56 +0330

    Deleting the create-auto-variables.py file from the source code

--- create-auto-variables.py
+++
@@ -1,9 +0,0 @@
-#Create as many variables as you want
-
-list1 = [10 , 20 , 30 , 40 , 50 , 60]
-dict1 = {}
-
-for i in range(1 , len(list1)+1):
-    number_name = f"num{i}"
-    dict1[number_name] = list1[i-1]
-print(dict1)
